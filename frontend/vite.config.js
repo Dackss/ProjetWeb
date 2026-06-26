cc2397203bea296c8340ca6b7a080173dbcef2e7
Dacks <thomas.geffroy@outlook.com>
2026-06-26 11:39:00 +0200

    config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
-export default defineConfig(({ command }) => ({
+export default defineConfig(() => ({
   plugins: [react(), tailwindcss()],
   resolve: {
     dedupe: ["react", "react-dom"],
@@ -13,5 +13,5 @@ export default defineConfig(({ command }) => ({
       usePolling: true,
     },
   },
-  base: command === 'build' ? '/app/' : '/',
+  base: '/',
 }));
